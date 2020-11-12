92b99f1e1c1c70d90594090a02a2dd55b19a2831
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-11-12 23:37:32 +0400

    Set up Expo, add basic elements to main page

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, TextInput, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={{ padding: 30 }}>
+			<View
+				style={{
+					flexDirection: "row",
+					justifyContent: "space-between",
+					alignItems: "center",
+				}}
+			>
+				<TextInput
+					placeholder="Course Goal"
+					style={{
+						borderBottomColor: "black",
+						borderBottomWidth: 1,
+						marginBottom: 5,
+						padding: 5,
+						width: "70%",
+					}}
+				/>
+				<Button title="ADD" style={{}} />
+			</View>
+			<View></View>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
