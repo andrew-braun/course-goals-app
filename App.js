2ce6f62b86678dbbef6d9584b2e9cc206380a617
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-11-13 16:05:17 +0400

    feat: Add item to list rendered in ScrollView

--- App.js
+++ App.js
@@ -1,32 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	TextInput,
+	View,
+	Button,
+	Text,
+	ScrollView,
+	FlatList,
+} from "react-native";
 
 export default function App() {
+	const [enteredGoal, setEnteredGoal] = useState("");
+	const [goalList, setGoalList] = useState([]);
+
+	const updateGoalInput = (textInput) => {
+		setEnteredGoal(textInput);
+	};
+
+	const updateGoalList = () => {
+		setGoalList((goalList) => [...goalList, enteredGoal]);
+		console.log(goalList);
+	};
+
 	return (
-		<View style={{ padding: 30 }}>
-			<View
-				style={{
-					flexDirection: "row",
-					justifyContent: "space-between",
-					alignItems: "center",
-				}}
-			>
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
 				<TextInput
 					placeholder="Course Goal"
-					style={{
-						borderBottomColor: "black",
-						borderBottomWidth: 1,
-						marginBottom: 5,
-						padding: 5,
-						width: "70%",
-					}}
+					style={styles.itemInput}
+					onChangeText={updateGoalInput}
+					value={enteredGoal}
 				/>
-				<Button title="ADD" style={{}} />
+				<Button title="ADD" onPress={updateGoalList} />
 			</View>
-			<View></View>
+			<ScrollView>
+				{goalList.map((goal, index) => (
+					<View style={styles.goalItem} key={index}>
+						<Text>{goal}</Text>
+					</View>
+				))}
+			</ScrollView>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	screen: {
+		padding: 50,
+	},
+	inputContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	itemInput: {
+		borderBottomColor: "black",
+		borderBottomWidth: 1,
+		marginBottom: 5,
+		padding: 5,
+		width: "70%",
+	},
+	goalItem: {
+		margin: 5,
+		padding: 10,
+		borderColor: "black",
+		borderWidth: 2,
+		borderRadius: 5,
+		backgroundColor: "#ccc",
+	},
+});
